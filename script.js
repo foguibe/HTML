24e225e74085dd577b90ee0ab1f2f9b2a5160b99
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-09-20 13:17:16 +0100

    changes

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     const sidebar = document.getElementById('sidebar');
     const menu = document.getElementById('menu');
+    const mode = document.getElementById('mode');
+    const body = document.getElementById('body');
 
     menu.addEventListener('click', function() {
         sidebar.classList.toggle('hidden');
